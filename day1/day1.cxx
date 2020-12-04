83ab22761b23f12a11522bb946cdad035af218ed
Noah Settersten <noah@headway.io>
2020-12-04 13:08:11 -0600

    Finish Day1 exercises
    (Terribly inefficient, but I'd like to catch up first.)

--- day1/day1.cxx
+++ day1/day1.cxx
@@ -8,6 +8,32 @@ void usage() {
   cout << "day1 input.txt\n";
 }
 
+int sumOf2(vector<int> expenses) {
+  for (size_t i = 0; i < expenses.size(); i++) {
+    for (size_t j = 0; j < expenses.size(); j++) {
+      if (expenses[i] + expenses[j] == 2020) {
+        return expenses[i] * expenses[j];
+      }
+    }
+  }
+
+  return -1;
+}
+
+int sumOf3(vector<int> expenses) {
+  for (size_t i = 0; i < expenses.size(); i++) {
+    for (size_t j = 0; j < expenses.size(); j++) {
+      for (size_t k = 0; k < expenses.size(); k++) {
+        if (expenses[i] + expenses[j] + expenses[k] == 2020) {
+          return expenses[i] * expenses[j] * expenses[k];
+        }
+      }
+    }
+  }
+
+  return -1;
+}
+
 int main(int argc, char **argv) {
   if (argc < 2) {
     cout << "Missing input file argument.\n";
@@ -26,9 +52,11 @@ int main(int argc, char **argv) {
 
     file.close();
 
-    cout << "Input data:\n";
-    for (size_t i = 0; i < expenses.size(); i++) {
-      cout << expenses[i] << ", ";
+    int result = sumOf3(expenses);
+    if (result < 0) {
+      cout << "Failed to find numbers that add up to 2020\n";
+    } else {
+      cout << "Multiplication of numbers that sum 2020: " << result << "\n";
     }
   } else {
     cout << "Failed to read file!";
